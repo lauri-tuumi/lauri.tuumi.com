c7a168efac33741cb39690f8f42e62782bcba25a
Lauri Tuumi <tundraltv@gmail.com>
2026-03-26 22:01:10 +0200

    Add root layout.tsx with metadataBase

---
+++ src/app/layout.tsx
@@ -0,0 +1,13 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://lauri.tuumi.com"),
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
